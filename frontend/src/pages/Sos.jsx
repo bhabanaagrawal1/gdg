655fd6859b3c07190e0b994abb9824efdb3d7823
BHABANA AGRAWAL <bhabanaagrawal1111@gmail.com>
2026-01-10 21:55:05 +0530

    Simplify Sos component by removing auth checks
    Removed authentication check and navigation logic.

--- frontend/src/pages/Sos.jsx
+++ frontend/src/pages/Sos.jsx
@@ -1,20 +1,11 @@
 import React, { useEffect } from 'react'
 import Qsos from '../components/Qsos'
-import { useAuth } from "../context/Auth-context";
-
-import { useNavigate } from 'react-router-dom'
 
 const Sos = () => {
   const { isAuth, authToken } = useAuth();
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!isAuth) {
-      navigate("/");
-    }
-  }, [isAuth, navigate]);
-
-  if (!isAuth) return null;
 
   return (
     <div>

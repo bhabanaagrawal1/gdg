0f4a6e30f502419f7fd863dafa73806379a93b8e
BHABANA AGRAWAL <bhabanaagrawal1111@gmail.com>
2026-01-10 21:52:56 +0530

    Update Setting.jsx

--- frontend/src/pages/Setting.jsx
+++ frontend/src/pages/Setting.jsx
@@ -1,21 +1,8 @@
 import React, { useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import Settings from '../components/Settings'
-import { useAuth } from "../context/Auth-context";
-
-import { useNavigate } from 'react-router-dom'
 
 const Setting = () => {
-  const { isAuth, authToken } = useAuth();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!isAuth) {
-      navigate("/");
-    }
-  }, [isAuth, navigate]);
-
-  if (!isAuth) return null;
 
   return (
     <div>

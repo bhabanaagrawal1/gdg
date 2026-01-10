0e5f316739fc632723ab9995cf4a029240e27211
BHABANA AGRAWAL <bhabanaagrawal1111@gmail.com>
2026-01-10 21:48:57 +0530

    Update FriendMapPage.jsx

--- frontend/src/pages/FriendMapPage.jsx
+++ frontend/src/pages/FriendMapPage.jsx
@@ -1,26 +1,16 @@
 import React, { useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import FriendMap from '../components/FriendMap'
-import { useAuth } from "../context/Auth-context";
-import { useNavigate } from "react-router-dom";
+
 
 const FriendMapPage = () => {
-    const { isAuth } = useAuth();
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        if (!isAuth) {
-            navigate("/");
-        }
-    }, [isAuth, navigate]);
-
-    if (!isAuth) return null;
 
     return (
         <div>
             <Navbar />
-            <div className="pt-24 px-4 pb-10 max-w-7xl mx-auto">
-                <h1 className="text-3xl font-bold mb-6 text-center lg:text-left">Live Friend Map</h1>
+            <div className="bg-linear-to-br from-[#f4f8fc] to-[#eef3f9]">
+                <h1 className="text-3xl lg:text-4xl font-bold mb-2 pt-15 text-center">Live Friend <span className='text-[#a7c7e7]'>Map</span></h1>
+                <h2 className='text-center text-gray-500 text-[14px] px-10 lg:px-70'><span className='hidden lg:block'>See your friends’ live locations when they’re online or in SOS mode.</span> The map also shows all friends who’ve activated SOS, so you stay connected when it matters most.</h2>
                 <FriendMap />
             </div>
         </div>

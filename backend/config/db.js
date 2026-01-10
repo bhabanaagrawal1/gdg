2f961b00fa2a64ca23cea2906e32e08fb0900ea3
iampinaki1 <pinaki82499730@gmail.com>
2026-01-10 07:17:20 +0530

    updated connect code

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    const conn = await mongoose.connect(`${process.env.MONGODB_URL}`);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {

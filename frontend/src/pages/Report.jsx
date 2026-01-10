e7f67c6fbbc370f0eae3d4c943f541f2d9e9962f
BHABANA AGRAWAL <bhabanaagrawal1111@gmail.com>
2026-01-10 21:51:58 +0530

    Update Report.jsx

--- frontend/src/pages/Report.jsx
+++ frontend/src/pages/Report.jsx
@@ -1,21 +1,13 @@
 import React, { useEffect } from 'react'
 import Reports from '../components/Reports'
 import Navbar from '../components/Navbar'
-import { useAuth } from "../context/Auth-context";
 
-import { useNavigate } from 'react-router-dom'
 
 const Report = () => {
   const { isAuth, authToken } = useAuth();
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!isAuth) {
-      navigate("/");
-    }
-  }, [isAuth, navigate]);
-
-  if (!isAuth) return null;
   return (
     <div>
       <Navbar />
